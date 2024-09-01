8dc0f7e2ccf7656c22cb5931a937a0616530dff5
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-01 18:50:30 +0300

    Connect API

--- src/Components/BookCard/BookCard.tsx
+++ src/Components/BookCard/BookCard.tsx
@@ -1,16 +1,31 @@
-import styles from './BookCard.module.css'
-export default function(){
-    return(
-        <div className={styles.card}>
-            <p>id:</p>
-            <p>Title:</p>
-            <p>Author:</p>
-            <p>Genre:</p>
-            <p>Year:</p>
-            <p>Checked out:</p>
-            <p>Permanent collection:</p>
-            <p>Created At:</p>
-        </div>
+import styles from './BookCard.module.css';
 
-    )
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  genre: string;
+  yearPublished: number;
+  checkedOut: boolean;
+  isPermanentCollection: boolean;
+  createdAt: string;
+}
+
+interface BookCardProps {
+  book: Book;
+}
+
+export default function BookCard({ book }: BookCardProps) {
+  return (
+    <div className={styles.card}>
+      <p>Id: {book.id}</p>
+      <p>Title: {book.title}</p>
+      <p>Author: {book.author}</p>
+      <p>Genre: {book.genre}</p>
+      <p>Year: {book.yearPublished}</p>
+      <p>Checked out: {book.checkedOut ? 'Yes' : 'No'}</p>
+      <p>Permanent collection: {book.isPermanentCollection ? 'Yes' : 'No'}</p>
+      <p>Created At: {book.createdAt}</p>
+    </div>
+  );
 }
--- src/pages/Browse/Browse.tsx
+++ src/pages/Browse/Browse.tsx
@@ -1,40 +1,52 @@
 import styles from './Browse.module.css'
 import BookCard from '../../Components/BookCard/BookCard'
 import { useEffect, useState } from 'react'
-export  function Browse(){
-    const BASE_URL="https://library-api.postmanlabs.com";
-    interface Book{
-        id:string;
+export function Browse() {
+    //not that it's a private key, I just did this for practice
+    require('dotenv').config();
+    const BASE_URL = process.env.api_key;
+
+    interface Book {
+        id: string;
         title: string;
-        author:string;
-        genre:string;
-        yearPublished:number;
-        checkedOut:boolean;
-        isPermanentCollection:boolean;
-        createdAt:Date;
+        author: string;
+        genre: string;
+        yearPublished: number;
+        checkedOut: boolean;
+        isPermanentCollection: boolean;
+        createdAt: string;
 
     }
-    const [books, setBooks]=useState<Book[]>([]);
-    useEffect(()=>{
-        const fetchBooks=async()=>{
-            const response=await fetch(`${BASE_URL}/books`,{headers:{"apikey":"postmanrulz"}});
-            const books=await response.json() as Book[];
-            setBooks(books);
-        }
-        fetchBooks()
+    const [books, setBooks] = useState<Book[]>([]);
+
+    useEffect(() => {
+
+        const myHeaders = new Headers();
+        myHeaders.append("Accept", "application/json");
+
+        const requestOptions: RequestInit = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow",
+            // Remove or change mode: 'no-cors' to 'cors'
+        };
+
+        fetch(`https://cors-anywhere.herokuapp.com/${BASE_URL}/books`, requestOptions)
+            .then((response) => response.json())
+            .then((result) => {console.log(result);
+            setBooks(result);
+            })
+            .catch((error) => console.error('Error:', error));
 
     }, [])
     return (
         <>
             <div className={styles.browseWrapper}>
                 <div className={styles.browseBox}>
-                    <BookCard></BookCard>
-                    <BookCard></BookCard>
-                    <BookCard></BookCard>
-                    <BookCard></BookCard>
-                    <BookCard></BookCard>
-                    <BookCard></BookCard>
-                   
+                   { books.map((book)=>{
+                    return <BookCard book={book}/>
+                   })}
+
                 </div>
             </div>
         </>
