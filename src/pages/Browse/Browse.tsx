f6074337ce6c228e322bfd71dd014e01c025d31a
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-01 17:10:30 +0300

    Test

--- src/pages/Browse/Browse.tsx
+++ src/pages/Browse/Browse.tsx
@@ -1,6 +1,29 @@
 import styles from './Browse.module.css'
 import BookCard from '../../Components/BookCard/BookCard'
+import { useEffect, useState } from 'react'
 export  function Browse(){
+    const BASE_URL="https://library-api.postmanlabs.com";
+    interface Book{
+        id:string;
+        title: string;
+        author:string;
+        genre:string;
+        yearPublished:number;
+        checkedOut:boolean;
+        isPermanentCollection:boolean;
+        createdAt:Date;
+
+    }
+    const [books, setBooks]=useState<Book[]>([]);
+    useEffect(()=>{
+        const fetchBooks=async()=>{
+            const response=await fetch(`${BASE_URL}/books`,{headers:{"apikey":"postmanrulz"}});
+            const books=await response.json() as Book[];
+            setBooks(books);
+        }
+        fetchBooks()
+
+    }, [])
     return (
         <>
             <div className={styles.browseWrapper}>
